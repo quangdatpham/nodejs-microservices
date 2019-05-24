db8bc26761636d2611143ff58cc45022192f78ad
Quang Dat <quangdat2000.pham@gmail.com>
2019-05-24 11:24:11 +0700

    initialize microservices project

---
+++ micro-admin/index.js
@@ -0,0 +1,9 @@
+/*eslint no-console: ["error", { allow: ["log"] }] */
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is now running on ${PORT}`);
+})
